0ce266898607c8bc26393fbe93634fb4ad6fb543
Gabe Morris <gabemorris1042@gmail.com>
2023-03-08 20:09:12 -0400

    initial commit

---
+++ config/connection.js
@@ -0,0 +1,10 @@
+
+const { connect } = require('mongoose');
+
+const connectionString =
+  process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/socialNetworkDB';
+
+connect(connectionString, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
---
+++ models/User.js
@@ -0,0 +1,22 @@
+
+const { model } = require('mongoose');
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    thoughts: []
+  }
+)
+
+const User = model('user', userSchema);
+module.exports = User;
